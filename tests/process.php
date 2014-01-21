844ef1898623730e9ab56432854ca2980f7a1801
Bill Patrianakos <bill@chooseclever.com>
2014-01-21 13:28:53 -0600

    Updated htaccess/htpasswd files.

--- tests/process.php
+++ tests/process.php
@@ -2,15 +2,16 @@
 
 $data = $_POST['contents'];
 
-
 $file = 'file.html';
 // Open the file to get existing content
 $current = file_get_contents($file);
 // Append a new person to the file
-$current .= $data . "\n";
+$current .= $data . "<br />" . "\n";
 // Write the contents back to the file
 file_put_contents($file, $current);
 
+// Uncomment the lines below if this test fails to write to the file
+// Make sure to comment out the above code (lines 5 - 11) if using the below code
 /*
 $myFile = "file.html";
 $fh = fopen($myFile, 'w') or die("can't open file");
